8fa1a5037e14c8f2fd031611d24f2df1aad66b74
Julien Bonté <j.bonte@groupeonepoint.com>
2020-11-02 22:03:56 -0500

    suite test remote config

--- MoisturizeMe/Moisturizer.h
+++ MoisturizeMe/Moisturizer.h
@@ -2,10 +2,11 @@
 #define MOISTURIZER_H
 
 #include <ArduinoJson.h>
-StaticJsonDocument<200> doc;
-
 #include "MoistureSensor.h";
 #include "MoistureValve.h";
+#include "RemoteDebug.h"
+RemoteDebug Debug;
+#include "EEPROM.h"
 
 class Moisturizer {
     public:
@@ -13,20 +14,34 @@ class Moisturizer {
         }
 
         void setup() {
-          
+//          DynamicJsonDocument doc(2048);
+//          String configString = EEPROM.readString(0);
+//          debugA("Yo %d", configString );
+//          // {"_id":"0","controller":{"id":0,"wifiSSid":"BELL693","wifiPass":"EF3A7CC4","mqttServer":"192.168.2.37","name":"Controller test"},"moisturizers":[{"id":0,"sensorPin":36,"waterValue":1000,"airValue":3450,"minMoistRatio":30,"valvePin":39,"valveTimer":2500,"name":"Moisturizer test"}]}
+//          if(sizeof(configString) > 0) {
+//            deserializeJson(doc, configString);
+//            JsonObject config = doc.as<JsonObject>();
+//            moisturizers = config["moisturizers"].as<JsonArray>();
+//            }
         }
 
         void loop() {
-
+          if(millis() - targetTime >= PERIOD) {
+            targetTime += PERIOD;
+            for(JsonVariant moisturizer: moisturizers) {
+              MoistureSensor sensor(moisturizer["sensorPin"].as<int>(), moisturizer["waterValue"].as<int>(), moisturizer["airValue"].as<int>(), moisturizer["minMoistRatio"].as<int>());
+              debugA("HEY %d", sensor.currentValue() );
+            }
+          }
         }
 
         void config(String content) {
           pinMode(LED_BUILTIN, OUTPUT);
-          delay(500);
           digitalWrite(LED_BUILTIN, HIGH);
           delay(500);
           digitalWrite(LED_BUILTIN, LOW);
-//          ESP.restart();
+//          EEPROM.writeString(0, content);
+          setup();
         }
 
         JsonArray print() {
@@ -37,6 +52,11 @@ class Moisturizer {
 //          doc["raw"] = raw();
 //          doc["value"] = humidity();
         }
+        
+    private:
+      JsonArray moisturizers;
+      unsigned long PERIOD = 1*60*1000UL;
+      unsigned long targetTime = 0UL;
 };
 
 #endif
